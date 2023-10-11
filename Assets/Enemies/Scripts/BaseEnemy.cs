da48ed0a440c4f161d99c0c699caab6b22b1b12e
Drackor15 <c3pjed10@gmail.com>
2023-10-11 16:19:04 -0600

    Added A* Pathfinding Library. Enemies can now pathfind to the Player when the player is within range.

--- Assets/Enemies/Scripts/BaseEnemy.cs
+++ Assets/Enemies/Scripts/BaseEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
+using Pathfinding;
 
 /// <summary>
 /// A base class for all basic enemies. Classes that inherit this class should
@@ -13,14 +14,27 @@ public class BaseEnemy : MonoBehaviour {
     protected Rigidbody2D rb;
     protected BoxCollider2D coll;
     protected float dirX = 0f;
+    protected Path path;
+    protected Seeker seeker;
+    protected int currentWaypoint;
+    protected bool reachedEndOfPath;
+    protected bool isChasing;
+    protected float pathfindingRefreshTimer;
 
+    [Header("Player Detection")]
     [SerializeField] protected LayerMask playerLayer;
+    [SerializeField] protected Transform playerTransform;
+    [SerializeField] protected float pathfindingUpdateFrequency = 0.2f;
+    [SerializeField] protected float nextWaypointDistance;
+    [SerializeField] protected float playerDetectRange;
+    [SerializeField] protected float chaseMultiplier = 1f;
 
+    [Header("Enemy Stats")]
     [SerializeField] protected int hp;
     [SerializeField] protected float moveSpeed;
     [SerializeField] protected float jumpForce;
-    [SerializeField] protected float playerDetectRange;
-    [SerializeField] protected float playerChaseRange;
+
+    [Header("Attack Stats")]
     [SerializeField] protected int attackDMG;
     [SerializeField] protected Vector2 attackRange;
     [SerializeField] protected Vector2 attackSize;
@@ -31,43 +45,102 @@ public class BaseEnemy : MonoBehaviour {
     protected virtual void Start() {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        seeker = GetComponent<Seeker>();
+
     }
 
     protected virtual void Update() {
-        ChasePlayer();
+        TryUpdatePathToPlayer();
+        EnemyAI();
     }
 
     #region Player Detection Methods
     /// <summary>
-    /// Checks for the Player within a certain radius.
-    /// Override this method to use an alternative detection shape.
+    /// If Player is detected, then increase Detection radius & Chase;
+    /// Otherwise decrease Detection radius & stop moving
     /// </summary>
-    /// <returns></returns>
-    protected virtual bool IsPlayerDetected() {
-        return Physics2D.CircleCast(coll.bounds.center, playerDetectRange, Vector2.left, playerDetectRange, playerLayer);
-    }
-
-    protected virtual bool IsPlayerInAttackRange() {
-        return Physics2D.BoxCast(coll.bounds.center, new Vector2(attackRange.x, attackRange.y), 0f, Vector2.left, 0f, playerLayer);
+    protected virtual void EnemyAI() {
+        if(IsPlayerDetected()) {
+            IncreaseDetectRadius();
+            ChasePlayer();
+        }
+        else {
+            DecreaseDetectRadius();
+        }
     }
 
     /// <summary>
-    /// If Player is detected, cast a larger circle & chase the player.
+    /// Checks for the Player within a certain radius.
+    /// Override this method to use an alternative detection shape.
+    /// </summary>
+    /// <returns>returns <see langword="true"/> if Player is in radius;
+    /// returns <see langword="false"/> if Player is not</returns>
+    protected virtual bool IsPlayerDetected() {
+        return Physics2D.CircleCast(coll.bounds.center, playerDetectRange, Vector2.left, 0f, playerLayer);
+    }
+
+    /// <summary>
+    /// Increases Detection Radius Once
+    /// </summary>
+    protected virtual void IncreaseDetectRadius() {
+        if(!isChasing) {
+            playerDetectRange *= chaseMultiplier;
+            isChasing = true;
+        }
+    }
+
+    /// <summary>
+    /// Decreases Detection Radius Once
+    /// </summary>
+    protected virtual void DecreaseDetectRadius() {
+        if(isChasing) {
+            playerDetectRange /= chaseMultiplier;
+            isChasing = false;
+        }
+    }
+
+    /// <summary>
+    /// Chase the Player until in Attack Range.
+    /// Once the Player is in Attack Range, stop moving & attack.
     /// </summary>
     protected virtual void ChasePlayer() {
-        if(IsPlayerDetected()) {
-            if(IsPlayerInAttackRange()) {
-                //Attack();
-            }
-            else {
-                // Find a path to the player's position
-                //List<Vector2> path = FindPathToPlayer();
+        if(IsPlayerInAttackRange()) {
+            //Attack();
+        }
+        else {
+            // Find a path to the player's position
+            //List<Vector2> path = FindPathToPlayer();
 
-                //if(path.Count > 0) {
-                    // Move towards the next waypoint in the path
-                    //MoveTo(path[0]);
-                //}
-            }
+            //if(path.Count > 0) {
+            // Move towards the next waypoint in the path
+            //MoveTo(path[0]);
+            //}
+        }
+    }
+
+    protected virtual void TryUpdatePathToPlayer() {
+        pathfindingRefreshTimer += Time.fixedDeltaTime;
+
+        if(pathfindingRefreshTimer >= pathfindingUpdateFrequency) {
+            pathfindingRefreshTimer = 0f;
+            UpdatePathToPlayer(); // Request pathfinding update
+        }
+    }
+
+    protected virtual void UpdatePathToPlayer() {
+        if(IsPlayerDetected() && !IsPlayerInAttackRange()) {
+            seeker.StartPath(rb.position, playerTransform.position, OnPathComplete);
+        }
+    }
+
+    /// <summary>
+    /// Inits path variables as long as nothing went wrong with path generation.
+    /// </summary>
+    /// <param name="p"></param>
+    protected virtual void OnPathComplete(Path p) {
+        if(!p.error) {
+            path = p;
+            currentWaypoint = 0;
         }
     }
 
@@ -83,6 +156,15 @@ public class BaseEnemy : MonoBehaviour {
     #endregion
 
     #region Enemy Attack Methods
+    /// <summary>
+    /// Checks if the Player is within the enemy's attack range
+    /// </summary>
+    /// <returns> returns <see langword="true"/> if Player is in attack range;
+    /// returns <see langword="false"/> if Player is not</returns>
+    protected virtual bool IsPlayerInAttackRange() {
+        return Physics2D.BoxCast(coll.bounds.center, new Vector2(attackRange.x, attackRange.y), 0f, Vector2.left, 0f, playerLayer);
+    }
+
     protected void Attack() {
         
     }
@@ -99,9 +181,6 @@ public class BaseEnemy : MonoBehaviour {
             // Draw Detect Radius
             Gizmos.color = Color.blue;
             Gizmos.DrawWireSphere(coll.bounds.center, playerDetectRange);
-            // Draw Chase Radius
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(coll.bounds.center, playerChaseRange);
 
             //// Draw Attack Box
             Gizmos.color = Color.red;
