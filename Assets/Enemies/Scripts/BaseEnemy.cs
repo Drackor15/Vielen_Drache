7a41944105226fcd4ec394f9caf16049d8114d73
Drackor15 <c3pjed10@gmail.com>
2023-10-11 20:18:09 -0600

    Enemies now walk towards and jump at the player. Jumping is wonky AF, though.

--- Assets/Enemies/Scripts/BaseEnemy.cs
+++ Assets/Enemies/Scripts/BaseEnemy.cs
@@ -32,7 +32,8 @@ public class BaseEnemy : MonoBehaviour {
     [Header("Enemy Stats")]
     [SerializeField] protected int hp;
     [SerializeField] protected float moveSpeed;
-    [SerializeField] protected float jumpForce;
+    [SerializeField] protected Vector2 maxLeap;
+    [SerializeField] private LayerMask jumpableGround;
 
     [Header("Attack Stats")]
     [SerializeField] protected int attackDMG;
@@ -54,7 +55,7 @@ public class BaseEnemy : MonoBehaviour {
         EnemyAI();
     }
 
-    #region Player Detection Methods
+    #region Enemy AI Methods
     /// <summary>
     /// If Player is detected, then increase Detection radius & Chase;
     /// Otherwise decrease Detection radius & stop moving
@@ -69,6 +70,24 @@ public class BaseEnemy : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Chase the Player until in Attack Range.
+    /// Once the Player is in Attack Range, stop moving & attack.
+    /// </summary>
+    protected virtual void ChasePlayer() {
+        if(IsPlayerInAttackRange()) {
+            //Attack();
+        }
+        else if(DoesNeedToLeap() && IsGrounded()) {
+            LeapTo();
+        }
+        else {
+            MoveOnPath();
+        }
+    }
+    #endregion
+
+    #region Player Detection Methods
     /// <summary>
     /// Checks for the Player within a certain radius.
     /// Override this method to use an alternative detection shape.
@@ -100,35 +119,22 @@ public class BaseEnemy : MonoBehaviour {
     }
 
     /// <summary>
-    /// Chase the Player until in Attack Range.
-    /// Once the Player is in Attack Range, stop moving & attack.
+    /// Update Path to Player after a fixed amount of time.
     /// </summary>
-    protected virtual void ChasePlayer() {
-        if(IsPlayerInAttackRange()) {
-            //Attack();
-        }
-        else {
-            // Find a path to the player's position
-            //List<Vector2> path = FindPathToPlayer();
-
-            //if(path.Count > 0) {
-            // Move towards the next waypoint in the path
-            //MoveTo(path[0]);
-            //}
-        }
-    }
-
     protected virtual void TryUpdatePathToPlayer() {
         pathfindingRefreshTimer += Time.fixedDeltaTime;
 
         if(pathfindingRefreshTimer >= pathfindingUpdateFrequency) {
             pathfindingRefreshTimer = 0f;
-            UpdatePathToPlayer(); // Request pathfinding update
+            UpdatePathToPlayer();
         }
     }
 
+    /// <summary>
+    /// Updates Path to Player if not already doing so.
+    /// </summary>
     protected virtual void UpdatePathToPlayer() {
-        if(IsPlayerDetected() && !IsPlayerInAttackRange()) {
+        if(IsPlayerDetected() && !IsPlayerInAttackRange() && seeker.IsDone()) {
             seeker.StartPath(rb.position, playerTransform.position, OnPathComplete);
         }
     }
@@ -143,15 +149,86 @@ public class BaseEnemy : MonoBehaviour {
             currentWaypoint = 0;
         }
     }
+    #endregion
 
-    //DUMMY
-    protected List<Vector2> FindPathToPlayer() {
-        return new List<Vector2>();
+    #region Enemy Movement Methods
+    /// <summary>
+    /// Adds force in the direction of the next waypoint on the path
+    /// </summary>
+    protected virtual void MoveOnPath() {
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        rb.AddForce(new Vector2(Mathf.RoundToInt(direction.x) * moveSpeed * (Time.deltaTime * 100), rb.velocity.y));
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWaypointDistance) {
+            currentWaypoint++;
+        }
     }
 
-    //DUMMY
-    protected void MoveTo(List<Vector2> path) {
-    
+    protected virtual void LeapTo() {
+        // Calculate the point on the A* path where the enemy should leap.
+        Vector3 leapPoint = CalculateLeapPoint();
+
+        // Calculate the vector force needed to reach the leap point.
+        Vector2 leapForce = CalculateLeapForce(leapPoint);
+
+        // Apply the force to the enemy's rigidbody to make it leap.
+        rb.AddForce(leapForce, ForceMode2D.Impulse);
+    }
+
+    // Calculate the point on the A* path where the enemy should leap.
+    protected virtual Vector3 CalculateLeapPoint() {
+        // You can use your pathfinding algorithm to find a point on the path
+        // that is within the range defined by maxLeap.x and maxLeap.y.
+        // For example, you can loop through the path waypoints and find the
+        // first waypoint that is at an appropriate distance from the enemy.
+
+        // Here is a simplified example. You can modify this as needed.
+        for(int i = 0; i < path.vectorPath.Count; i++) {
+            Vector3 waypoint = path.vectorPath[i];
+            float distanceX = Mathf.Abs(waypoint.x - transform.position.x);
+            float distanceY = Mathf.Abs(waypoint.y - transform.position.y);
+
+            if(distanceX <= maxLeap.x && distanceY <= maxLeap.y) {
+                return waypoint;
+            }
+        }
+
+        // If no suitable point is found, return the last waypoint as a fallback.
+        return path.vectorPath[path.vectorPath.Count - 1];
+    }
+
+    // Calculate the vector force needed to reach the leap point.
+    protected virtual Vector2 CalculateLeapForce(Vector3 leapPoint) {
+        // Calculate the direction from the current position to the leap point.
+        Vector2 direction = (leapPoint - transform.position).normalized;
+
+        // Calculate the force needed to reach the leap point based on the distance.
+        float distance = Vector3.Distance(transform.position, leapPoint);
+        Vector2 leapForce = direction * moveSpeed * distance;
+
+        return leapForce;
+    }
+
+    /// <summary>
+    /// Subtracts Enemy's Transform Height from Player's Transform Height
+    /// to determine if the Enemy needs to jump.
+    /// </summary>
+    /// <returns>returns <see langword="true"/> if difference is greater than 3
+    /// ; otherwise returns <see langword="false"/></returns>
+    protected virtual bool DoesNeedToLeap() {
+        if(playerTransform.position.y - transform.position.y > 3) {
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Creates a Box Collider (equal in size to the Enemy BoxCollider)
+    /// slightly below the Enemy collider to detect if the enemy is on jumpable ground.
+    /// </summary>
+    /// <returns>True if on jumpable ground; False if anywhere else</returns>
+    private bool IsGrounded() {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
     #endregion
 
