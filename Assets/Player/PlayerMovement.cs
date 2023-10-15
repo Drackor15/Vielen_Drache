d5785d735ef2dadc93649449e402ddc07a52e4aa
DSU <MaritimeMesmer@gmail.com>
2023-10-15 05:44:21 -0500

    click somewhere around this to read more the jump is too heavy atm for a princess, will tweak more - min height between 1 and 2 looks right - logic for variable height single jump (I'm close to okay with the buffer && duration) - mid jump with double jump hits 5, high jump with double jump hits 6 height - - with how it is rn, you get a higher jump from jumping before apexes, hope to change this - triple jump to hit 9, if exists
    my main consideration is that we could nuke triple jump
    make double jump behind an ability and also add a timer-ed no gravity 0-yvel float thing
    - like peach jump+down in ssb games
    
    this consideration would propgate the idea that switching between abilities quickly
    is the way to navigate certain tight areas
    
    didnt mess with xvel, did remove the accel because im gonna fudge it tmrw
    - i like it but it was taking too long to hit max
    probably with just speedup < n being faster than speedup >= n

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -1,6 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
+
+public enum PlayerPower
+{
+    None,
+    Smash,
+    Mobility
+}
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -23,14 +32,17 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
     private Animator anim;
     private float dirX = 0f;
-    
-    private bool doublejump;
-    private bool powerG = true;
+
+    private bool isGrounded = false;
+    private int holdingJumpCounter = 0;
+    private bool canDoubleJump = false;
+    private bool canTripleJump = false;
+
+    PlayerPower playerPower = PlayerPower.Mobility;
 
     [SerializeField] private LayerMask jumpableGround;
 
-    [SerializeField] private float moveSpeed = 11f;
-    [SerializeField] private float jumpForce = 30f;
+    [SerializeField] private float moveSpeed = 11.0f;
     [SerializeField] private float grav = 7.5f;
     #endregion
 
@@ -47,49 +59,56 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame.
     private void Update()
     {
+        updatePlayerState();
         HandlePlayerInput();
         UpdatePlayerAnimation();
     }
 
     #region Update Methods
-    /// <summary>
-    /// Updates Player Movement based on Player Input
-    /// </summary>
-    private void HandlePlayerInput() {
-        /* "Horizontal" is used instead of hardcoding left and right keys.
-         * rb.velocity.y and rb.velocity.x are used in place of 0 to not cut off momentum/set the speed to 0.
-         * Input.GetAxisRaw("Horizontal") would stop the slide/deacceleration effect.*/
-        dirX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-        
-        /* If spacebar is pressed, access sprite rigidbody2D component, and make it jump.
-         * HARDCODED --> GetKey("space") = Continuous, GetKeyDown("space") = Only once.
-         * VERSATILE --> GetButton("Jump") = Continuous, GetButtonDown("Jump") = Only once.
-         * GetButtons connects to Unity, allowing better control schemes (multiple buttons can do the same thing).*/
-        if(Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            /* Vector3 is a data holder for 3 different values (X, Y, and Z coordinates). In this case, Vector2 is just X and Y values.
-             * You must specify how much speed/velocity goes into each direction X and Y. --> (Don't do Z usually because it's depth!)*/
-            doublejump = true;
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-        else if(Input.GetButtonDown("Jump") && doublejump == true)
-        {
-            doublejump = false;
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce * 2/3f );
-        }
-        else if (Input.GetButtonDown("Jump") && doublejump == false && powerG == true)
-        {
-            powerG = false;
-            rb.gravityScale = grav / 1.5f;
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce * 1.4f);
-            
-        }
 
-        if (IsGrounded() && powerG == false)
+    private void updatePlayerState()
+    {
+        isGrounded = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);   // investigate
+        if ( isGrounded )
         {
-            powerG = true;
-            rb.gravityScale = grav;
+            canDoubleJump = true;
+            canTripleJump = true;
+        }
+    }
+
+    private void HandlePlayerInput()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");  // -1, 0, 1
+
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                Debug.Log("What");
+                holdingJumpCounter = 27;
+                rb.velocity = new Vector2(rb.velocity.x, 16);
+            }
+            else if (canDoubleJump)
+            {
+                canDoubleJump = false;
+                rb.velocity = new Vector2(rb.velocity.x, 25);
+            }
+            else if (playerPower == PlayerPower.Mobility && canTripleJump)
+            {
+                canTripleJump = false;
+                rb.velocity = new Vector2(rb.velocity.x, 27);
+            }
+        }
+        else if (Input.GetButtonUp("Jump"))
+            holdingJumpCounter = 0;
+        else if (holdingJumpCounter > 0)
+        {
+            Debug.Log("Here");
+            if (holdingJumpCounter > 12)
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + 0.75f);
+            --holdingJumpCounter;
         }
     }
 
@@ -98,26 +117,13 @@ public class PlayerMovement : MonoBehaviour
     /// (currently this is just based on player movement, but once this gets larger we may
     /// want to consider adding seperate helper functions for different conditions such as death & dmg, powerups, etc.)
     /// </summary>
-    private void UpdatePlayerAnimation() {
-        // Updates player based on movement along the X-Axis
-        if(dirX > 0f) {
+    private void UpdatePlayerAnimation()
+    {
+        if (dirX > 0f)
             sprite.flipX = false;
-        }
-        else if(dirX < 0f) {
+        else if (dirX < 0f)
             sprite.flipX = true;
-        }
-        else {
-        
-        }
     }
 
-    /// <summary>
-    /// Creates a Box Collider (equal in size to the Player BoxCollider)
-    /// slightly below the Player collider to detect if the player is on jumpable ground.
-    /// </summary>
-    /// <returns>True if on jumpable ground; False if anywhere else</returns>
-    private bool IsGrounded() {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
-    }
     #endregion
 }
