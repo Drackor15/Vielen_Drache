29d99e63c5752713f659e70e5a5a4c7f6c654423
Drackor15 <c3pjed10@gmail.com>
2023-10-15 22:27:31 -0600

    removed enemy jumping methods - will instead get to work on patroling. Capped enemy velocity. Changed enemy collision type from Discrete to Continuous.

--- Assets/Enemies/Scripts/BaseEnemy.cs
+++ Assets/Enemies/Scripts/BaseEnemy.cs
@@ -32,8 +32,7 @@ public class BaseEnemy : MonoBehaviour {
     [Header("Enemy Stats")]
     [SerializeField] protected int hp;
     [SerializeField] protected float moveSpeed;
-    [SerializeField] protected Vector2 maxLeap;
-    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] protected Vector2 maxSpeed;
 
     [Header("Attack Stats")]
     [SerializeField] protected int attackDMG;
@@ -51,6 +50,7 @@ public class BaseEnemy : MonoBehaviour {
     }
 
     protected virtual void Update() {
+        ClampVelocity();
         TryUpdatePathToPlayer();
         EnemyAI();
     }
@@ -77,9 +77,6 @@ public class BaseEnemy : MonoBehaviour {
     protected virtual void ChasePlayer() {
         if(IsPlayerInAttackRange()) {
             //Attack();
-        }
-        else if(DoesNeedToLeap() && IsGrounded()) {
-            LeapTo();
         }
         else {
             MoveOnPath();
@@ -153,6 +150,15 @@ public class BaseEnemy : MonoBehaviour {
 
     #region Enemy Movement Methods
     /// <summary>
+    /// Makes sure that Enemy X and Y velocities don't exceed maxSpeed.x & maxSpeed.y
+    /// </summary>
+    protected virtual void ClampVelocity() {
+        float clampedX = Mathf.Clamp(rb.velocity.x, -maxSpeed.x, maxSpeed.x);
+        float clampedY = Mathf.Clamp(rb.velocity.y, -maxSpeed.y, maxSpeed.y);
+        rb.velocity = new Vector2(clampedX, clampedY);
+    }
+
+    /// <summary>
     /// Adds force in the direction of the next waypoint on the path
     /// </summary>
     protected virtual void MoveOnPath() {
@@ -162,73 +168,6 @@ public class BaseEnemy : MonoBehaviour {
         if(distance < nextWaypointDistance) {
             currentWaypoint++;
         }
-    }
-
-    protected virtual void LeapTo() {
-        // Calculate the point on the A* path where the enemy should leap.
-        Vector3 leapPoint = CalculateLeapPoint();
-
-        // Calculate the vector force needed to reach the leap point.
-        Vector2 leapForce = CalculateLeapForce(leapPoint);
-
-        // Apply the force to the enemy's rigidbody to make it leap.
-        rb.AddForce(leapForce, ForceMode2D.Impulse);
-    }
-
-    // Calculate the point on the A* path where the enemy should leap.
-    protected virtual Vector3 CalculateLeapPoint() {
-        // You can use your pathfinding algorithm to find a point on the path
-        // that is within the range defined by maxLeap.x and maxLeap.y.
-        // For example, you can loop through the path waypoints and find the
-        // first waypoint that is at an appropriate distance from the enemy.
-
-        // Here is a simplified example. You can modify this as needed.
-        for(int i = 0; i < path.vectorPath.Count; i++) {
-            Vector3 waypoint = path.vectorPath[i];
-            float distanceX = Mathf.Abs(waypoint.x - transform.position.x);
-            float distanceY = Mathf.Abs(waypoint.y - transform.position.y);
-
-            if(distanceX <= maxLeap.x && distanceY <= maxLeap.y) {
-                return waypoint;
-            }
-        }
-
-        // If no suitable point is found, return the last waypoint as a fallback.
-        return path.vectorPath[path.vectorPath.Count - 1];
-    }
-
-    // Calculate the vector force needed to reach the leap point.
-    protected virtual Vector2 CalculateLeapForce(Vector3 leapPoint) {
-        // Calculate the direction from the current position to the leap point.
-        Vector2 direction = (leapPoint - transform.position).normalized;
-
-        // Calculate the force needed to reach the leap point based on the distance.
-        float distance = Vector3.Distance(transform.position, leapPoint);
-        Vector2 leapForce = direction * moveSpeed * distance;
-
-        return leapForce;
-    }
-
-    /// <summary>
-    /// Subtracts Enemy's Transform Height from Player's Transform Height
-    /// to determine if the Enemy needs to jump.
-    /// </summary>
-    /// <returns>returns <see langword="true"/> if difference is greater than 3
-    /// ; otherwise returns <see langword="false"/></returns>
-    protected virtual bool DoesNeedToLeap() {
-        if(playerTransform.position.y - transform.position.y > 3) {
-            return true;
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// Creates a Box Collider (equal in size to the Enemy BoxCollider)
-    /// slightly below the Enemy collider to detect if the enemy is on jumpable ground.
-    /// </summary>
-    /// <returns>True if on jumpable ground; False if anywhere else</returns>
-    private bool IsGrounded() {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
     #endregion
 
